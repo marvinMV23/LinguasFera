2c55aa34bb04e04c70c046367eb5b18b9d49a2f7
MarcosMineiro <marcus.mineiro2016@gmail.com>
2025-01-18 19:54:51 -0300

    novos ajustes

--- main.js
+++ main.js
@@ -6,10 +6,13 @@
 
 document.querySelectorAll('.toggle-button').forEach(button => {
     button.addEventListener('click', () => {
-        const content = button.nextElementSibling;
+        const content = button.parentElement.nextElementSibling;
         const isVisible = content.style.display === 'block';
 
+        // Alterna a exibição da caixa clicada
         content.style.display = isVisible ? 'none' : 'block';
-        button.textContent = isVisible ? 'Expandir' : 'Recolher';
+        button.classList.toggle('uil-angle-down', isVisible);
+        button.classList.toggle('uil-angle-up', !isVisible);
     });
 });
+
